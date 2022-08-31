9df7970661eaba5b1ab64e1e455121992aeb43b4
Tim Schaub <tim@planet.com>
2022-08-31 15:39:22 -0600

    Unnecessary code

--- rollup.config.js
+++ rollup.config.js
@@ -14,14 +14,4 @@ export default {
     cjs(),
     production && terser()
   ],
-  onwarn: function(warning, superOnWarn) {
-    /*
-     * skip certain warnings
-     * https://github.com/openlayers/openlayers/issues/10245
-     */
-    if (warning.code === 'THIS_IS_UNDEFINED') {
-      return;
-    }
-    superOnWarn(warning);
-  }
 };
